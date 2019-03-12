234dd965d70c6a092c5a09c1cbe734c05d99b5a0
dateutli <daviidalberto@gmail.com>
2019-03-11 20:33:41 -0600

    testing GPS

--- public/upload.php
+++ public/upload.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","my_user","my_password","my_db");
+    $conn = mysqli_connect("localhost","admin","1123#Jpgr","test");
 
     $lat = mysqli_real_escape_string($conn, $_POST['lat']);
     $lng = mysqli_real_escape_string($conn, $_POST['lng']);
